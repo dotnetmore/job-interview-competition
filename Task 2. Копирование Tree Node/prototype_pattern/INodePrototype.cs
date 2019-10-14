4f8c6244e1841cbba3e64d6eab694e76e46e0c1f
Ivan <gud-fil@mail.ru>
2019-10-14 09:38:35 +0300

    Task 3.
    	Задание: Написать класс копирования нода дерева. Как по ссылке так и по значению.
    	Решение: Реализовал с помощью паттерна "Прототип". Результат работы -приложен скриншот вывода в консоль.

---
+++ Task 2. Копирование Tree Node/prototype_pattern/INodePrototype.cs
@@ -0,0 +1,15 @@
+﻿using System.Dynamic;
+
+namespace prototype_pattern
+{
+    public interface INodePrototype
+    {
+
+        int Index { get; set; }
+        string Name { get; set; }
+
+        UserInfo UserInfo { get; set; }
+
+        INodePrototype Clone(bool isDeepCopi);
+    }
+}
---
+++ Task 2. Копирование Tree Node/prototype_pattern/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace prototype_pattern
+{
+    [Serializable]
+    public class UserInfo
+    {
+        public int Age {get;set;}
+        public string Name {get;set;}
+    }
+}
